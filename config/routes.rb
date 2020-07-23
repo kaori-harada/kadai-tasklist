98863ba880ee5919b63bf1ae3fa8578ec1b3b49e
kaori.H <kor20150829@gmail.com>
2020-07-23 13:55:30 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/destroy'
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create, :destroy]
   resources :tasks
 
 end
